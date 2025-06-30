227c2a13e55299c49f5808a608d4b327ad47dac7
pedr88jesus <pedro.picholi.pereira@escola.pr.gov.br>
2025-06-29 22:57:02 -0300

    ok

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-// Efeito de balão subindo (extra)
+// BALÕES FLUTUANTES
 window.onload = () => {
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 6; i++) {
     const balao = document.createElement("img");
     balao.src = "https://i.ibb.co/ZKbb06W/balao.png";
     balao.className = "balao-flutuante";
     balao.style.left = `${Math.random() * 100}vw`;
-    balao.style.animationDuration = `${5 + Math.random() * 5}s`;
+    balao.style.animationDuration = `${5 + Math.random() * 4}s`;
     document.body.appendChild(balao);
+
+    // Remover após subir
+    setTimeout(() => {
+      balao.remove();
+    }, 10000);
   }
 };
