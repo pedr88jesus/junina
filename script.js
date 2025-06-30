1dfa68ce05a36746dca5dda5ac85601bd2043e58
pedr88jesus <pedro.picholi.pereira@escola.pr.gov.br>
2025-06-30 11:59:56 -0300

    ok

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 window.onload = () => {
   function criarBalao() {
     const balao = document.createElement("img");
-    balao.src = "https://i.ibb.co/ZKbb06W/balao.png";
+    balao.src = "balao1.png";
     balao.className = "balao-flutuante";
     balao.style.left = `${Math.random() * 100}vw`;
     balao.style.animationDuration = `${5 + Math.random() * 5}s`;
